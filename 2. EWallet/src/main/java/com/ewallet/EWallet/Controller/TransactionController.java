ea3783b37076d0fc432fc31a4eb6ecf77fca03a7
Manish Shah <77191841+manishshah7974@users.noreply.github.com>
2022-09-20 02:34:30 +0530

    Update TransactionController.java
    Added @Transactional annotation to Rollback transactions if Runtime Exception occurs

--- 2. EWallet/src/main/java/com/ewallet/EWallet/Controller/TransactionController.java
+++ 2. EWallet/src/main/java/com/ewallet/EWallet/Controller/TransactionController.java
@@ -1,4 +1,5 @@
 package com.ewallet.EWallet.Controller;
+
 import com.ewallet.EWallet.Model.AddBalanceDetails;
 import com.ewallet.EWallet.Model.Transaction;
 import com.ewallet.EWallet.Model.User;
@@ -10,12 +11,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-//import javax.transaction.Transactional;
+
+import javax.transaction.Transactional;
+
 @RestController
+@Transactional                           // To Rollback the transaction if Runtime Exception occurs like SQLException
 @RequestMapping("/transaction")
 public class TransactionController {
 
-    @Autowired                               // Using another Application .
+    @Autowired                           // Using another Microservice(User) to validate if user exists or not 
     private UserService userService;
     @Autowired
     private WalletRepository walletRepository;
@@ -26,65 +30,67 @@ public class TransactionController {
 
     @PostMapping("/sendMoney")
     public Transaction sendMoney(@RequestBody Transaction transaction) throws Exception {
-        /* From Request Transaction Body , we get sender ID and receiver ID     */
+
+        /* From Request Transaction Body , we get sender ID and receiver ID */
         User sender = userService.findUserById(transaction.getSender_id());
         User receiver = userService.findUserById(transaction.getReceiver_id());
 
         if (sender == null || receiver == null) {
-            logger.info("Either RECEIVER OR SENDER Is Null {},{}", sender.toString(),receiver.toString());
+            logger.info("Either RECEIVER OR SENDER Is Null {},{}", sender.toString(), receiver.toString());
             throw new Exception("Bad PAYLOAD");
         }
+
         Wallet senderWallet = walletRepository.findByUserId(sender.getId());
         Wallet receiverWallet = walletRepository.findByUserId(receiver.getId());
 
         int amount = transaction.getAmount();
         if (senderWallet.getBalance() < amount) {
-            logger.info("Current Balance {} is less than Required Balance i.e {}",senderWallet.getBalance(),amount);
+            logger.info("Current Balance {} is less than Required Balance i.e {}", senderWallet.getBalance(), amount);
             throw new Exception("INSUFFICIENT BALANCE ");
         }
-            senderWallet.setBalance(senderWallet.getBalance() - amount);
-            receiverWallet.setBalance(receiverWallet.getBalance() + amount);
-            transaction.setStatus(1);          //  1 - SuccessFul   0 - UnSuccessFul
-            walletRepository.save(senderWallet);
-            walletRepository.save(receiverWallet);
-            logger.info("Transaction Was Successful ");
-            // EMAIL SERVICE
-            try{
-              userService.sendEmail("Amount deducted from sender "+sender.getMobile()+"is "+amount);
-              userService.sendEmail("Amount credited to receiver "+receiver.getMobile()+"is "+amount);
 
-            }catch (Exception exception){
-                logger.info("Email Service Is Down {}",exception.getStackTrace());
-            }
+        senderWallet.setBalance(senderWallet.getBalance() - amount);
+        receiverWallet.setBalance(receiverWallet.getBalance() + amount);
+        transaction.setStatus(1);                         // 1 - SuccessFul 0 - UnSuccessFul
+        walletRepository.save(senderWallet);
+        walletRepository.save(receiverWallet);           
+        
+        logger.info("Transaction Was Successful ");
+        logger.info("Amount deducted from sender " + sender.getMobile() + "is " + amount);
+        logger.info("Amount credited to receiver " + receiver.getMobile() + "is " + amount);
 
-            return transactionRepository.save(transaction);
+        // Email Service linked to Apache Kafka to Send Notification
+        try {
+            userService.sendEmail("Amount deducted from sender "+sender.getMobile()+" is "+amount);
+            userService.sendEmail("Amount credited to receiver "+receiver.getMobile()+" is "+amount);
+        } catch (Exception exception) {
+            logger.info("Email Service Is Down {}", exception.getStackTrace());
         }
 
+        return transactionRepository.save(transaction);         // Transaction Completes here 
+    }
 
-
-
-
-    //addBalance
-    //request ---- user id, amount
-    //wallet already have 100 rs, add 10
-    //customername
-    @PutMapping("/addBalance")
-    public Integer addBalance(@RequestBody AddBalanceDetails addBalanceDetails){
+    // addBalance
+    // request body ---- { user id, amount }
+    // Authentication Required to access this API by Admin only to add balance to users 
+    
+    @PutMapping("/addBalance")              
+    public Integer addBalance(@RequestBody AddBalanceDetails addBalanceDetails) {
         logger.info("Got this request for adding balance : {}", addBalanceDetails.toString());
-        //find wallet by user id
+        // find wallet by user id
         Wallet wallet = walletRepository.findByUserId(addBalanceDetails.getUserid());
-        //set balance in the wallet
+        // set balance in the wallet
         wallet.setBalance(addBalanceDetails.getAmount() + wallet.getBalance());
         walletRepository.save(wallet);
         return wallet.getBalance();
     }
 
-    //adminname
+    
     @GetMapping("/getBalance/{userid}")
-    public Integer getBalance(@PathVariable int userid) throws Exception{
+    public Integer getBalance(@PathVariable int userid) throws Exception {
         Wallet wallet = walletRepository.findByUserId(userid);
-        if(wallet == null){
-            throw new Exception("Wallet is not created for this user : "+userid);
+        if (wallet == null) {
+            throw new Exception("Wallet is not created for this user : " + userid);
         }
         return wallet.getBalance();
     }
